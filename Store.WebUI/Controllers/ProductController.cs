bd34bab4632fef5ab7f20c265d0c93884658ea76
Patryk Szylin <patrykszylin93@gmail.com>
2018-02-02 20:21:35 +0000

    First run of the application. Setup up NinjectDependencyResolver, modified layout file and added mock data.

---
+++ Store.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store.Domain.Abstract;
+using Store.Domain.Entities;
+
+namespace Store.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        IProductsRepository _repository;
+
+        public ProductController(IProductsRepository productRepository)
+        {
+            this._repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
---
+++ Store.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using Moq;
+using System.Web.Mvc;
+using Store.Domain.Abstract;
+using Store.Domain.Entities;
+
+namespace Store.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            _kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            // put bindings here.
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name= "Football", Price = 25 },
+                new Product {Name = "Surf Board", Price = 179 },
+                new Product {Name = "Running Shoes", Price = 95 }
+            });
+
+            _kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+        }
+
+    }
+}
