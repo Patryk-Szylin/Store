fd6630d80fc51553c756d1bc30444ea0c1e6923a
Patryk Szylin <patrykszylin93@gmail.com>
2018-02-03 11:46:39 +0000

    Savepoint: Saving the project before changing System.Web.Mvc version from 5.2.3.0 to 5.0.0.0 for the Store.WebUI project

--- Store.UnitTests/UnitTest1.cs
+++ Store.UnitTests/UnitTest1.cs
@@ -1,5 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+using Moq;
+using Store.Domain.Abstract;
+using Store.Domain.Entities;
+using System.Web.Mvc;
+using Store.WebUI.Controllers;
+using Store.WebUI.Models;
+using Store.WebUI.HtmlHelpers;
 
 namespace Store.UnitTests
 {
@@ -7,8 +16,26 @@ namespace Store.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Generate_Page_Links()
         {
+            //Arrange - define an html helper - we need to do tihs
+            // in order to apply the 
+
+            HtmlHelper myHelper = null;
+
+            // Arrange - create paginginfo data
+            PagingInfo pi = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Arrange - set up the delegate using a lambda expression
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // Act
+            //MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
         }
     }
 }
--- Store.WebUI/Controllers/ProductController.cs
+++ Store.WebUI/Controllers/ProductController.cs
@@ -5,21 +5,39 @@ using System.Web;
 using System.Web.Mvc;
 using Store.Domain.Abstract;
 using Store.Domain.Entities;
+using Store.WebUI.Models;
 
 namespace Store.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         IProductsRepository _repository;
+        public int m_pageSize = 4;
 
         public ProductController(IProductsRepository productRepository)
         {
             this._repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(_repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = _repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * m_pageSize)
+                .Take(m_pageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = m_pageSize,
+                    TotalItems = _repository.Products.Count()
+                }
+            };
+
+            return View(model);
+
         }
     }
 }
