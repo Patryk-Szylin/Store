05664373f6880600632912e24bf692a8beaf6cdb
Patryk Szylin <patrykszylin93@gmail.com>
2018-02-05 13:59:08 +0000

    CART: Added remove button that allows users to remove products from their cart

---
+++ Store.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store.Domain.Entities;
+
+namespace Store.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            //Get cart from the session
+            Cart cart = null;
+            if(controllerContext.HttpContext.Session != null)
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            }
+
+            // Create the cart if there wasn't one in the session data
+            if(cart == null)
+            {
+                cart = new Cart();
+                if(controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+
+            // return the cart
+            return cart;
+        }
+
+    }
+}
