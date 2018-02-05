854e3d79b356f1048fa07c8d5271ecab7a9531ef
Patryk Szylin <patrykszylin93@gmail.com>
2018-02-05 12:18:27 +0000

    Added page link highlight to illustrate which category users are currently viewing

---
+++ Store.UnitTests/UnitTest2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+using Moq;
+using Store.Domain.Abstract;
+using Store.Domain.Entities;
+using System.Web.Mvc;
+using Store.WebUI.Controllers;
+using Store.WebUI.Models;
+using Store.WebUI.HtmlHelpers;
+
+namespace Store.UnitTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"}
+            });
+        }
+    }
+}
--- Store.WebUI/Controllers/NavController.cs
+++ Store.WebUI/Controllers/NavController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Store.Domain.Abstract;
 
 namespace Store.WebUI.Controllers
 {
     public class NavController : Controller
-    { 
-        public string Menu()
+    {
+        private IProductsRepository _repository;
+
+        public NavController(IProductsRepository repo)
         {
-            return "Hello fron navbar";
+            _repository = repo;
+        }
+
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+
+            IEnumerable<string> categories = _repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return PartialView(categories);
         }
 
     }
