b7d917ec05c85c28f8fdd00189266237d9d52bc4
Patryk Szylin <patrykszylin93@gmail.com>
2018-02-07 15:31:43 +0000

    Added basic admin page: Editable products

--- Store.Domain/Entities/Product.cs
+++ Store.Domain/Entities/Product.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Store.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]                 // this tells the MVC to render the property as hidden form element
         public int ProductID { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]                  // and this allows me to specify how value is represented and edited
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
---
+++ Store.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store.Domain.Abstract;
+using Store.Domain.Entities;
+
+namespace Store.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductsRepository _repository;
+
+        public AdminController(IProductsRepository repo)
+        {
+            _repository = repo;
+        }
+
+
+        public ViewResult Index()
+        {
+            return View(_repository.Products);
+        }
+
+        public ViewResult Edit(int productID)
+        {
+            var product = _repository.Products.FirstOrDefault(p => p.ProductID == productID);
+            return View(product);
+        }
+
+    }
+}
